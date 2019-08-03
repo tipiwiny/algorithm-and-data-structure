1285c352a5b8ea106600745d7ca422c15effb5a5
Juan Luis Perez <tipiwiny@gmail.com>
2019-08-03 21:10:47 +0200

    capitalize exercise

---
+++ capitalize/index.js
@@ -0,0 +1,22 @@
+function capitalize1(str) {
+  const words= []
+  for(let word of str.split(' '))
+   {
+     words.push(word[0].toUpperCase() + word.slice(1));
+   }
+   return words.join(' ')
+}
+
+function capitalize2(str) {
+  let result = str[0].toUpperCase();
+  for(let i =1; i < str.length; i++) {
+    if(str[i- 1] == ' ') {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
+
+module.exports = {capitalize1, capitalize2};
